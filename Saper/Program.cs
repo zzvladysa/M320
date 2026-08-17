51d1ccd2dcacb27d34b29024496cb1fbd8908f64
zzvladysa <zzvladysa@gmail.com>
2026-08-17 12:11:30 +0200

    hello

---
+++ Saper/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Saper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowCount = 5;
+            int ColumnCount = 5;
+            bool[,] mineField = new bool[rowCount, ColumnCount];
+
+            PrintBoard(mineField);
+
+            while (true)
+            {
+                Console.WriteLine("Bitte Feld eingeben: ");
+                var field = Console.ReadLine();
+
+                if (field == "q")
+                {
+                    break;
+                }
+
+                UncoverField(mineField, field);
+                PrintBoard(mineField);
+            }
+        }
+
+        static void PrintBoard(bool[,] board)
+        {
+            Console.WriteLine("   A  B  C  D  E");
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                Console.Write($"{i + 1}  ");
+
+                for (int k = 0; k < board.GetLength(1); k++)
+                {
+                    if (!board[i, k])
+                    {
+                        Console.Write("X  ");
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void UncoverField(bool[,] board, string field)
+        {
+            var rowChar = field.Substring(0, 1);
+            var row = (int)char.Parse(rowChar);
+            row -= 65;
+
+            var colChar = field.Substring(1);
+            var col = int.Parse(colChar);
+            col -= 1;
+
+            board[row, col] = true;
+        }
+    }
+}
---
+++ aufgabe2/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace aufgabe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = MyMath.ReadInt();
+            int b = MyMath.ReadInt();
+
+            int ggt = MyMath.Calc_ggT(a, b);
+            int kgv = MyMath.Calc_kgV(a, b);
+
+            MyMath.ShowResult("ggT", a, b, ggt);
+            MyMath.ShowResult("kgV", a, b, kgv);
+        }
+    }
+
+    public class MyMath
+    {
+        public static int Calc_ggT(int a, int b)
+        {
+            int z;
+
+            while (b != 0)
+            {
+                z = a % b;
+                a = b;
+                b = z;
+            }
+
+            return a;
+        }
+
+        public static int Calc_kgV(int a, int b)
+        {
+            return (a * b) / Calc_ggT(a, b);
+        }
+
+        public static int ReadInt()
+        {
+            int zahl;
+
+            do
+            {
+                Console.WriteLine("Bitte geben Sie eine Zahl ein: ");
+                String s = Console.ReadLine();
+                zahl = int.Parse(s);
+
+            } while (zahl <= 0);
+
+            return zahl;
+        }
+
+        public static void ShowResult(string name, int a, int b, int result)
+        {
+            Console.WriteLine(name + " von " + a + " und " + b + " ist " + result);
+        }
+    }
+}
